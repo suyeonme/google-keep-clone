2049142a5f8193e9241d9d8eeb44b4f27e41185e
Suyeon Kang <tndusrkd92@gmail.com>
2020-08-01 15:58:44 +0900

    Truncate note's length

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -4,11 +4,21 @@ import './Note.scss';
 import Icon from '../../icons/trash-can.svg';
 
 const Note = props => {
+    const truncateText = p => {
+        let paragraph;
+
+        if (p.length > 120) {
+            paragraph = p.substr(0, 120) + '...';
+        } else {
+            paragraph = p;
+        };
+        return paragraph;
+    };
 
     return(
         <div className="Note">
             <h1>{props.title}</h1>
-            <p>{props.content}</p>
+            <p>{truncateText(props.content)}</p>
             <button onClick={() => props.onRemove(props.id)}>
                 <img src={Icon} alt="Delete Icon"/>
             </button>
